a9a4d57d580fef2beae9a32563c9a4b567dee63a
Libor <libor.gess11@gmail.com>
2025-05-01 21:14:40 +0200

    tried to work with classes

---
+++ js/PictureClassInit.js
@@ -0,0 +1,8 @@
+import {Picture} from './PictureClass.js';
+
+const carousel = ''
+const arrowNext = document.querySelector('.arrowNext');
+const arrowPrev = document.querySelector('.arrowPrev');
+
+
+const picture = new Picture(arrowNext, arrowPrev);
